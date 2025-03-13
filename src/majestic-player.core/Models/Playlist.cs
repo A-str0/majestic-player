8279377c447d0ee26b7d61a6a6b0ee73b7c7b74c
A-str0 <il.eroxin@gmail.com>
2025-03-13 16:07:20 +0300

    Added PlaylistService and LibraryService

--- src/majestic-player.core/Models/Playlist.cs
+++ src/majestic-player.core/Models/Playlist.cs
@@ -1,9 +1,10 @@
 namespace majestic_player.core.Models
 {
-    public class Playlist
-    {
-        public Guid Id { get; } = Guid.NewGuid();
-        public required string Title { get; set; }
-        public List<Track> Tracks { get; } = new();
-    }
+public class Playlist
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public required string Name { get; set; }
+    public List<Track> Tracks { get; set; } = new List<Track>(); // Added property to hold tracks
+}
+
 }
