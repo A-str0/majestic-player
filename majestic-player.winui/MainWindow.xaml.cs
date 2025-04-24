9d3ad05788539a2c501900801a871666086b4062
A-str0 <il.eroxin@gmail.com>
2025-04-24 17:13:57 +0300

    `Created basic UI`

--- majestic-player.winui/MainWindow.xaml.cs
+++ majestic-player.winui/MainWindow.xaml.cs
@@ -12,6 +12,16 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using System;
+using System.Reactive;
+using System.Windows.Input;
+using ReactiveUI;
+using majestic_player.core.Models;
+using System.Threading.Tasks;
+using majestic_player.core.Interfaces;
+using majestic_player.infrastructure.Services;
+using Windows.Storage;
+using Microsoft.Extensions.DependencyInjection;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,14 +33,57 @@ namespace majestic_player.winui
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        #region Services
+        private readonly LibraryService? _libraryService;
+        private readonly IMediaHandlerService? _mediaHandlerService;
+        private readonly PlaybackQueueService? _playbackQueueService;
+        private readonly IAudioService? _audioService;
+        #endregion
+
+        #region Commands
+        public ReactiveCommand<Unit, Unit> AddFolderCommand { get; private set; }
+        public ReactiveCommand<Track?, Unit> PlayTrackCommand { get; private set; }
+        public ICommand PlayPauseCommand { get; private set; }
+        public ICommand NextCommand { get; private set; }
+        public ICommand PreviousCommand { get; private set; }
+        #endregion
+
         public MainWindow()
         {
+            Console.WriteLine("MainWindow initialization started");
+
+            // Setup Commands
+            //AddFolderCommand = ReactiveCommand.CreateFromTask(AddFolderDialog);
+            //PlayTrackCommand = ReactiveCommand.CreateFromTask<Track?>(PlayTrack);
+
+            //PlayPauseCommand = ReactiveCommand.Create(PlayPause);
+            NextCommand = ReactiveCommand.Create(PlayNextTrackInQueue);
+            PreviousCommand = ReactiveCommand.Create(PlayPreviousTrackInQueue);
+
+
+            // Setup Services
+            IServiceProvider serviceProvider = Program.Services.CreateScope().ServiceProvider;
+
+            _audioService = serviceProvider.GetRequiredService<IAudioService>();
+
+            _libraryService = serviceProvider.GetRequiredService<LibraryService>();
+            _mediaHandlerService = serviceProvider.GetRequiredService<IMediaHandlerService>();
+            _playbackQueueService = serviceProvider.GetRequiredService<PlaybackQueueService>();
+
+
             this.InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        public async Task? PlayNextTrackInQueue()
         {
-            myButton.Content = "Clicked";
+            Track? nextTrack = _playbackQueueService?.ToNextTrackInQueue();
+            await _audioService?.PlayAsync(nextTrack);
+        }
+
+        public async Task? PlayPreviousTrackInQueue()
+        {
+            Track? prevTrack = _playbackQueueService?.ToPreviousTrackInQueue();
+            await _audioService?.PlayAsync(prevTrack);
         }
     }
 }
